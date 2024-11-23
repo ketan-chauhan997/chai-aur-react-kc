e08e206f9678fa80ed90ec962ad046b9c1e30e1c
ketan-chauhan997 <chauhan.25997@gmail.com>
2024-11-23 17:24:34 +0530

    Video - 4 Custom React Project (Understanding the process how React works)

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <h1>My Custom App</h1>
+  )
+}
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  //JSX syntax
+  <MyApp/>
 )
